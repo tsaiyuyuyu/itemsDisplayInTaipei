90b38c59db147b6df12460d44738fd92e1ecf09a
tsaiyu <spagfish21@gmail.com>
2021-02-10 17:32:58 +0800

    新增資料夾

---
+++ js/text.js
@@ -0,0 +1,42 @@
+// 計數器
+var count = 1;
+var countEl = document.getElementById("count");
+function plus() {
+    count++;
+    countEl.value = count;
+}
+function minus() {
+    if (count > 1){
+        count--;
+        countEl.value = count;
+    }
+}
+
+// banner
+// var indaxValue = 1;
+//         showImg(indexValue);
+//         function dot_slide(e) {
+//             showImg((indaxValue = e));
+//         }
+//         function side_slide(e) {
+//             showImg((indaxValue += e));
+//         }
+//         function showImg(e) {
+//             var i;
+//             const img = document.querySelectorAll('.banner-content>img');
+//             const sliders = document.querySelectorAll('.dot');
+//             if (e > img.length) {
+//                 indaxValue = 1;
+//             }
+//             if (e < 1) {
+//                 indaxValue = img.length;
+//             }
+//             for (i = 0; i < img.length; i++) {
+//                 img[i].style.display = "none";
+//             }
+//             for (i = 0; i < sliders.length; i++) {
+//                 sliders[i].style.color = "red";
+//             }
+//             img[indaxValue - 1].style.display = "block";
+//             sliders[indaxValue - 1].style.color = "white";
+//         }
