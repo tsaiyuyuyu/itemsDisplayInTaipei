16c19910eaa4e7e4337aa56ed854d75a581e4f87
tsaiyu <spagfish21@gmail.com>
2021-02-10 10:54:54 +0800

    count的js

---
+++ text.js
@@ -0,0 +1,13 @@
+
+var count = 1;
+var countEl = document.getElementById("count");
+function plus() {
+    count++;
+    countEl.value = count;
+}
+function minus() {
+    if (count > 1){
+        count--;
+        countEl.value = count;
+    }
+}
